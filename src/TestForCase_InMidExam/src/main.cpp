fd01813f57cddc45cb482baff3af72fbfe51a325
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-01-20 20:56:20 +0700

    testcase

---
+++ src/TestForCase_InMidExam/src/main.cpp
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+uint16_t Count16;
+int main()
+{
+    DDRD |= (1 << PD4) ; 
+    TCCR1A = 0b00000000;
+    TCCR1B = 0b00000101;
+    TCNT1 = 0;
+    PORTD &= ~(1 << PD4); 
+    while (true)
+    {
+        Count16 = TCNT1;
+        if ((Count16 > 62500)) // check (4 second) ?
+        {
+            PORTD ^= (1 << PD4); //(XOR)
+            TCNT1 = 0;
+        }
+    }
+}
+
