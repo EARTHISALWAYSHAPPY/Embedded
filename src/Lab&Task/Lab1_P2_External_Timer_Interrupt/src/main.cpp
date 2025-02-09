2ae68f53ee34dbcb64e63e2d29da1f47fe0d5b18
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-02-10 01:34:00 +0700

    backup_code

--- src/Lab&Task/Lab1_P2_External_Timer_Interrupt/src/main.cpp
+++ src/Lab&Task/Lab1_P2_External_Timer_Interrupt/src/main.cpp
@@ -2,20 +2,27 @@
 ///////////////
 #include <avr/io.h>
 #include <util/delay.h>
-
+#define F_CPU 16000000UL
 #define INT0_vect _VECTOR(1)
 #define INT1_vect _VECTOR(2)
 #define TIMER1_OVF_vect _VECTOR(13)
+
+#define pin_red PD4
+#define pin_yellow PD5
+#define pin_green PD6
 
 // variable:
 bool dir = 0;
 uint8_t speed = 0, color = 0;
 // speed : 0 --> 1 --> 2 --> 3 --> 4 --> 0 (+ 1:200 ms)
 // color : red(0) , yellow(1) , green(2)
+uint16_t speed_use = 200, timer;
+char speed_text[20];
 
 void setting_port()
 {
   // red(D4) , yellow(D5) , green(D6)
+  DDRD |= ((1 << pin_green) | (1 << pin_yellow) | (1 << pin_red));
 }
 void setting_interrupt()
 {
@@ -25,7 +32,7 @@ void setting_interrupt()
   EIMSK |= (1 << INT1) | (1 << INT0); // enable INT1 , INT0
 
   TCCR1A = 0b00000000;
-  TCCR1B = 0b00000101;
+  TCCR1B = 0b00000101; // prescale 1024
   TIMSK1 = 0b00000001;
 }
 
@@ -35,6 +42,10 @@ void serial_begin(int baudrate)
   UCSR0A = 0b00000000;
   UCSR0B = 0b10011000;
   UCSR0C = 0b00000110;
+}
+void clear()
+{
+  PORTD &= ~((1 << pin_green) | (1 << pin_yellow) | (1 << pin_red));
 }
 void Serial_putc(char data)
 {
@@ -68,19 +79,86 @@ void Serial_puts(char *data)
 
 void serial_display()
 {
-  ///////////////////
+  switch (dir)
+  {
+  case 0:
+    Serial_puts("Red --> Yellow --> Green : ");
+    break;
+  case 1:
+    Serial_puts("Green --> Yellow --> Red : ");
+    break;
+  default:
+    break;
+  }
+  sprintf(speed_text, "%d", speed_use);
+  strcat(speed_text, "ms");
+  strcat(speed_text, "\r\n");
+  Serial_puts(speed_text);
 }
 ISR(INT0_vect) // for dir
 {
-  ///////////////////
+  dir ^= 1;
+  serial_display();
 }
 ISR(INT1_vect) // for speed : 0 --> 1 --> 2 --> 3 --> 4 --> 0
 {
-  ///////////////////
+  if (speed < 4)
+  {
+    speed++;
+  }
+  else
+    speed = 0;
+  speed_use = (speed + 1) * 200;
+  serial_display();
+}
+ISR(TIMER1_OVF_vect)
+{
+  float time_sec = speed_use / 1000.0;
+  timer = 65535 - (F_CPU / 1024) * time_sec;
+  TCNT1 = timer;
+
+  clear();
+  clear();
+  switch (dir)
+  {
+  case 0: // Red --> Yellow --> Green
+    switch (color)
+    {
+    case 0:
+      PORTD |= (1 << pin_red);
+      break;
+    case 1:
+      PORTD |= (1 << pin_yellow);
+      break;
+    case 2:
+      PORTD |= (1 << pin_green);
+      break;
+    }
+    break;
+  case 1: // Green --> Yellow --> Red
+    switch (color)
+    {
+    case 0:
+      PORTD |= (1 << pin_green);
+      break;
+    case 1:
+      PORTD |= (1 << pin_yellow);
+      break;
+    case 2:
+      PORTD |= (1 << pin_red);
+      break;
+    }
+    break;
+  }
+  color = (color + 1) % 3;
 }
 
 int main()
 {
+  serial_begin(51);
+  setting_interrupt();
+  setting_port();
+  sei();
   while (1)
   {
   }
